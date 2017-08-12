494e5b1ae82d43534f7e7ea96fad7d4fbded2f75
shuaicj <shuaicj@gmail.com>
2017-08-12 21:30:25 +0800

    add rest-war: packaged as war

---
+++ rest-war/src/main/java/shuaicj/example/rest/war/Application.java
@@ -0,0 +1,26 @@
+package shuaicj.example.rest.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Spring boot app.
+ *
+ * @author shuaicj 2017/08/12
+ */
+@SpringBootApplication(scanBasePackages = {"shuaicj.example.rest.war", "shuaicj.example.rest.common"})
+public class Application extends SpringBootServletInitializer {
+
+    // for spring boot packaging: war
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
