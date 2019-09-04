8d039549ae4d65acf3e1e8a5833a22aa93298ee0
shuaicj <shuaicj@gmail.com>
2019-09-04 17:17:16 +0800

    upgrade to spring-boot-2.1.6

--- rest-war/src/main/java/shuaicj/example/rest/war/Application.java
+++ rest-war/src/main/java/shuaicj/example/rest/war/Application.java
@@ -3,7 +3,7 @@ package shuaicj.example.rest.war;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Spring boot app.
